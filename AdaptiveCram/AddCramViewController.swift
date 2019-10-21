e5a82e4206cfa63f3e92ec9407d7f525e938ca5f
naldaram <mos5656@naver.com>
2019-10-21 21:22:07 +0900

    백그라운드 색상 변경

---
+++ AdaptiveCram/AddCramViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddCramViewController.swift
+//  AdaptiveCram
+//
+//  Created by 문은서 on 21/10/2019.
+//
+
+import UIKit
+
+class AddCramViewController: UIViewController {
+
+    @IBOutlet weak var CurrentQuestionLabel: UILabel!
+    @IBOutlet weak var TotalQuestionLabel: UILabel!
+    @IBOutlet weak var questionITextField: UITextField!
+    @IBOutlet weak var answerSegmentControl: UISegmentedControl!
+    @IBOutlet weak var solutionTextField: UITextField!
+    @IBOutlet weak var addQuestionButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
